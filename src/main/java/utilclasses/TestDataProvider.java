0931ef897bc4c28609c52a690b21a0a144cbd482
Nitin Vilas More <nitin.more@espire.com>
2024-07-17 19:50:46 +0530

    First Maven Project

---
+++ src/main/java/utilclasses/TestDataProvider.java
@@ -0,0 +1,33 @@
+package utilclasses;
+
+import java.util.Map;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class TestDataProvider {
+
+	@DataProvider(name = "data")
+
+	public Object[][] readExcelData()
+
+	{
+		return ExcelReader.readData("C:\\Users\\nitin.more\\eclipse-workspace\\Automation_Practise\\testdataDemoqa.xlsx", "WebtablesElement");
+		//return new ExcelReader(getClass()).readData("C:\\Users\\nitin.more\\Desktop\\TestDataForMapping.xlsx","Sheet1");
+	}
+	
+//	@Test(dataProvider = "data")
+//
+//	public void m5(Map<String, String> input)
+//
+//	{
+//		System.out.println(input.get("firstName"));
+//		System.out.println(input.get("lastName"));
+//		System.out.println(input.get("userEmail"));
+//		System.out.println(input.get("age"));
+//		System.out.println(input.get("salary"));
+//		System.out.println(input.get("department"));
+//	}
+}
+
+
---
+++ src/test/java/testClasses/WebTablesTest.java
@@ -0,0 +1,27 @@
+package testClasses;
+
+import java.util.Map;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import utilclasses.ExcelReader;
+
+public class WebTablesTest extends BaseClass{
+	
+	@DataProvider(name = "data")
+
+	public Object[][] readExcelData()
+
+	{
+		return ExcelReader.readData("C:\\ws\\Automation_Practise\\testdataDemoqa.xlsx", "WebtablesElement");
+		//return new ExcelReader(getClass()).readData("C:\\Users\\nitin.more\\Desktop\\TestDataForMapping.xlsx","Sheet1");
+	}
+	
+	@Test (dataProvider = "data")
+	public void validatingWebTables(Map<String, String> input) throws Exception
+	{
+		wtb.webTablesElement(input);
+	}
+
+}
